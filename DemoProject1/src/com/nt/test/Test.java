6302ef5ea3eec9a6f02b88ae59757480df6b6d46
TL <TL@gmail.com>
2021-11-04 12:55:28 +0530

    Dev1 modified Test.java

--- DemoProject1/src/com/nt/test/Test.java
+++ DemoProject1/src/com/nt/test/Test.java
@@ -1,7 +1,11 @@
 package com.nt.test;
 
+import com.nt.beans.Demo;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello!");
+		Demo demo = new Demo();
+		System.out.println(demo.sysDate());
 	}
 }
